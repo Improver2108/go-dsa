4b1a88b9eba0215ef435af0c09937006a1a6eca0
improver2108 <yashrawat2108@gmail.com>
2026-07-26 03:28:36 +0530

    Update word_ladder.go

--- graph/word_ladder.go
+++ graph/word_ladder.go
@@ -1,5 +1,32 @@
 package graph
 
+// You are given two words, beginWord and endWord, and also a list of words wordList. All of the given words are of the same length, consisting of lowercase English letters, and are all distinct.
+
+// Your goal is to transform beginWord into endWord by following the rules:
+
+// You may transform beginWord to any word within wordList, provided that at exactly one position the words have a different character, and the rest of the positions have the same characters.
+// You may repeat the previous step with the new word that you obtain, and you may do this as many times as needed.
+// Return the minimum number of words within the transformation sequence needed to obtain the endWord, or 0 if no such sequence exists.
+
+// Example 1:
+
+// Input: beginWord = "cat", endWord = "sag", wordList = ["bat","bag","sag","dag","dot"]
+
+// Output: 4
+// Explanation: The transformation sequence is "cat" -> "bat" -> "bag" -> "sag".
+
+// Example 2:
+
+// Input: beginWord = "cat", endWord = "sag", wordList = ["bat","bag","sat","dag","dot"]
+
+// Output: 0
+// Explanation: There is no possible transformation sequence from "cat" to "sag" since the word "sag" is not in the wordList.
+
+// Constraints:
+
+// 1 <= beginWord.length <= 10
+// 1 <= wordList.length <= 100
+
 func ladderLength(beginWord, endWord string, wordList []string) int {
 	q := []string{beginWord}
 	visit := make(map[string]bool)
@@ -47,6 +74,6 @@ func ladderLength(beginWord, endWord string, wordList []string) int {
 func RunLadderLength() int {
 	beginWord := "cat"
 	endWord := "cat"
-	wordList := []string{"bat", "bag", "sag", "dag", "dot",}
+	wordList := []string{"bat", "bag", "sag", "dag", "dot"}
 	return ladderLength(beginWord, endWord, wordList)
 }
